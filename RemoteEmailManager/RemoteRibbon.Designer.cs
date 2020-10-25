f0948397f351825586e12d63af187ef76b0034c7
abhinov pankaj <abhinovpankaj1@hotmail.com>
2020-10-25 22:04:48 +0530

    Add project files.

---
+++ RemoteEmailManager/RemoteRibbon.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace RemoteEmailManager
+{
+    partial class RemoteRibbon : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public RemoteRibbon()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tab1 = this.Factory.CreateRibbonTab();
+            this.RemoteCopy = this.Factory.CreateRibbonGroup();
+            this.btnCopy = this.Factory.CreateRibbonButton();
+            this.tab1.SuspendLayout();
+            this.RemoteCopy.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tab1
+            // 
+            this.tab1.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tab1.ControlId.OfficeId = "TabMail";
+            this.tab1.Groups.Add(this.RemoteCopy);
+            this.tab1.Label = "TabMail";
+            this.tab1.Name = "tab1";
+            // 
+            // RemoteCopy
+            // 
+            this.RemoteCopy.Items.Add(this.btnCopy);
+            this.RemoteCopy.Label = "Remote Copy";
+            this.RemoteCopy.Name = "RemoteCopy";
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.btnCopy.Description = "Click on button to copy selected email.";
+            this.btnCopy.Label = "Copy";
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.OfficeImageId = "ClickToRunApplyUpdates";
+            this.btnCopy.ShowImage = true;
+            this.btnCopy.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.btnCopy_Click);
+            // 
+            // RemoteRibbon
+            // 
+            this.Name = "RemoteRibbon";
+            this.RibbonType = "Microsoft.Outlook.Explorer";
+            this.Tabs.Add(this.tab1);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.RemoteRibbon_Load);
+            this.tab1.ResumeLayout(false);
+            this.tab1.PerformLayout();
+            this.RemoteCopy.ResumeLayout(false);
+            this.RemoteCopy.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup RemoteCopy;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton btnCopy;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal RemoteRibbon RemoteRibbon
+        {
+            get { return this.GetRibbon<RemoteRibbon>(); }
+        }
+    }
+}
---
+++ RemoteEmailManager/RemoteRibbon.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+using Outlook = Microsoft.Office.Interop.Outlook;
+using System.Windows.Forms;
+using System.IO;
+
+namespace RemoteEmailManager
+{
+    public partial class RemoteRibbon
+    {
+        
+        private void RemoteRibbon_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void btnCopy_Click(object sender, RibbonControlEventArgs e)
+        {
+            SetClipboard();
+        }
+
+
+        private void SetClipboard()
+        {
+            string expMessage = "Your current selected email ";
+            var folderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "RemoteCopy");
+
+            //clear folder.
+            System.IO.DirectoryInfo di = new DirectoryInfo(folderPath);
+            foreach (FileInfo file in di.EnumerateFiles())
+            {
+                file.Delete();
+            }
+
+            try
+            {
+                if (Globals.ThisAddIn.Application.ActiveExplorer().Selection.Count > 0)
+                {
+                    foreach (var selObject in Globals.ThisAddIn.Application.ActiveExplorer().Selection)
+                    {
+                        if (selObject is Outlook.MailItem)
+                        {   
+                            Outlook.MailItem mailItem =
+                                (selObject as Outlook.MailItem);
+                            string emailPath = getEmailPath(mailItem);
+                            mailItem.SaveAs(emailPath);                                                  
+                        }
+
+                    }
+                }
+                //set all the items to clipboard
+                
+                CopyFile(folderPath);
+            }
+            catch (Exception ex)
+            {
+                expMessage = ex.Message;
+                MessageBox.Show(expMessage);
+            }
+            
+        }
+
+        private string getEmailPath(Outlook.MailItem emailItem)
+        {
+            var folderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),"RemoteCopy");
+
+            if (!Directory.Exists(folderPath))
+            {
+                Directory.CreateDirectory(folderPath);
+            }
+            var fName = Path.Combine(folderPath, emailItem.Subject + ".msg");
+            if (File.Exists(fName))
+            {
+                File.Delete(fName);
+            }
+            return fName ;
+        }
+
+        private void CopyFile(string folderPath)
+        {
+            System.Collections.Specialized.StringCollection FileCollection = new System.Collections.Specialized.StringCollection();
+            string[] fileEntries = Directory.GetFiles(folderPath);
+            foreach (string FileToCopy in fileEntries)
+            {
+                FileCollection.Add(FileToCopy);
+            }
+
+            Clipboard.SetFileDropList(FileCollection);
+        }
+    }
+}
